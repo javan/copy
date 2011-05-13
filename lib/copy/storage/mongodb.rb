ea17aab68046c80f1bce7d9610577b98e3825e96
Javan Makhmali <javan@javan.us>
2011-05-13 17:00:46 -0400

    fix #set

--- lib/copy/storage/mongodb.rb
+++ lib/copy/storage/mongodb.rb
@@ -1,27 +1,39 @@
 require 'mongo'
-require 'bson_ext'
 require 'uri'
 
 module Copy
   module Storage
     class Mongodb
       def initialize(connection_url)
-        uri  = URI.parse(connection_url)
-        conn = ::Mongo::Connection.from_uri(connection_url)
-        db   = conn.db(uri.path.gsub(/^\//, ''))
+        uri         = URI.parse(connection_url)
+        connection  = ::Mongo::Connection.from_uri(connection_url)
+        database    = conn.db(uri.path.gsub(/^\//, ''))
+        
         @collection = db['copy-content']
         @collection.ensure_index([['name', Mongo::ASCENDING]], :unique => true)
         @collection
       end
       
       def get(name)
-        doc = @collection.find('name' => name)
+        doc = find(name)
         doc['content'] unless doc.nil?
       end
       
       def set(name, content)
-        @collection.insert('name' => name, 'content' => content)
+        doc = find(name)
+        if doc
+          doc['content'] = content
+          @collection.update({ '_id' => doc['_id'] }, doc)
+        else
+          @collection.insert('name' => name, 'content' => content)
+        end
       end
+      
+      private
+        def find(name)
+          docs = @collection.find('name' => name)
+          docs.first if docs.respond_to?(:first)
+        end
     end
   end
 end
